0909db926e5d6a84c1e37dad5c92a528e3390434
Jack Lin <jackzlin@deloitte.com.cn>
2023-11-04 21:55:22 +0800

    Init project

---
+++ src/test/java/com/jack/train/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jack.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
