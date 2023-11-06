a61ba31c03fd7a6fc30a77fddd3095f845be4331
Jack Lin <jackzlin@deloitte.com.cn>
2023-11-06 21:49:17 +0800

    * add member module * create logback-spring.xml for member module

--- member/src/main/java/org/jack/train/member/config/MemberApplication.java
+++ member/src/main/java/org/jack/train/member/config/MemberApplication.java
@@ -1,14 +1,21 @@
 package org.jack.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("org.jack")
 public class MemberApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
-    }
 
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+
+    public static void main(String[] args) {
+        var ioc = SpringApplication.run(MemberApplication.class, args);
+        Environment env = ioc.getEnvironment();
+        LOG.info("{} started. Address: http://127.0.0.1:{}{}", env.getProperty("spring.application.name"), env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+    }
 }
