24556b0a9fac6d5b50ef8bea04dde87bffea3c20
Jack Lin <jackzlin@deloitte.com.cn>
2023-11-14 22:53:15 +0800

    return http status with 400 when having business error

--- common/src/main/java/org/jack/common/controller/ControllerExceptionHandler.java
+++ common/src/main/java/org/jack/common/controller/ControllerExceptionHandler.java
@@ -4,6 +4,8 @@ import org.jack.common.exception.BusinessException;
 import org.jack.common.response.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,16 +24,15 @@ public class ControllerExceptionHandler {
 
     @ExceptionHandler(value = BusinessException.class)
     @ResponseBody
-    public Response businessExceptionHandler(BusinessException e) {
+    public ResponseEntity<Response> businessExceptionHandler(BusinessException e) {
         LOG.error("business error: {}", e.getException().getDesc());
-        return Response.fail(e.getException().getDesc());
-
+        return new ResponseEntity<>(Response.fail(e.getException().getDesc()), HttpStatus.BAD_REQUEST);
     }
 
 
     @ExceptionHandler(value = BindException.class)
     @ResponseBody
-    public Response bindExceptionHandler(BindException e) {
+    public ResponseEntity<Response> bindExceptionHandler(BindException e) {
         var br = e.getBindingResult();
 
         var errors = br.getAllErrors();
@@ -42,7 +43,7 @@ public class ControllerExceptionHandler {
         }
 
         LOG.error("validation error: {}", sb);
-        return Response.fail(sb.toString());
+        return new ResponseEntity<>(Response.fail(sb.toString()), HttpStatus.BAD_REQUEST);
 
     }
 }
