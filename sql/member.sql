c431cee83c71897ea7b010cf74c18ef6d46f0764
Jack Lin <jackzlin@deloitte.com.cn>
2023-11-21 21:29:13 +0800

    add passenger table

--- sql/member.sql
+++ sql/member.sql
@@ -8,3 +8,18 @@ create table `member`
 
 ) engine = InnoDB
   default charset = utf8mb4 comment 'member table';
+
+
+drop table if exists `passenger`;
+create table `passenger` (
+    `id` bigint not null comment 'id',
+    `member_id` bigint not null comment 'member id',
+    `name` varchar(20) not null comment 'passenger name',
+    `id_card` varchar(18) not null comment 'passenger id card NO',
+    `type` char(1) not null comment 'passenger type|enum[PassengerTypeEnum]',
+    `create_time` datetime(3) comment 'create time of the record',
+    `update_time` datetime(3) comment 'update time of the record',
+    primary key (`id`),
+    index `member_id_index` (`member_id`)
+) engine = InnoDB
+  default charset = utf8mb4 comment 'passenger';
