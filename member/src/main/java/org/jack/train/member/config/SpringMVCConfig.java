a561337617ad76ed3407948a0ebfae925dd98fcd
Jack Lin <jackzlin@deloitte.com.cn>
2023-11-26 16:14:39 +0800

    add LogInterceptor to handle the log id

--- member/src/main/java/org/jack/train/member/config/SpringMVCConfig.java
+++ member/src/main/java/org/jack/train/member/config/SpringMVCConfig.java
@@ -1,6 +1,7 @@
 package org.jack.train.member.config;
 
 import jakarta.annotation.Resource;
+import org.jack.common.interceptor.LogInterceptor;
 import org.jack.common.interceptor.MemberInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,8 +12,14 @@ public class SpringMVCConfig implements WebMvcConfigurer {
     @Resource
     MemberInterceptor memberInterceptor;
 
+    @Resource
+    LogInterceptor logInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                        .addPathPatterns("/**");
+
         registry.addInterceptor(memberInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/member/sendCode", "/member/login");
