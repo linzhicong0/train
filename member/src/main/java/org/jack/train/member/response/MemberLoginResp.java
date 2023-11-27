f37e6dcc93283263c631d77be113ee2dcbfa4936
Jack Lin <jackzlin@deloitte.com.cn>
2023-11-27 21:37:19 +0800

    update the id for member and passenger to string type

--- member/src/main/java/org/jack/train/member/response/MemberLoginResp.java
+++ member/src/main/java/org/jack/train/member/response/MemberLoginResp.java
@@ -1,5 +1,7 @@
 package org.jack.train.member.response;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,6 +9,7 @@ import lombok.Data;
 @Builder
 public class MemberLoginResp {
 
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
 
     private String mobile;
--- member/src/main/java/org/jack/train/member/response/PassengerQueryResp.java
+++ member/src/main/java/org/jack/train/member/response/PassengerQueryResp.java
@@ -1,12 +1,16 @@
 package org.jack.train.member.response;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
 @Data
 public class PassengerQueryResp {
 
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
 
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long memberId;
 
     private String name;
